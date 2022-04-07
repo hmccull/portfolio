ca50c74229e8c3b980c2274a794da0fd2179f065
Hannah McCullough <hmac091@gmail.com>
2022-04-07 21:12:23 +0900

    social icons added, need to resize

--- src/components/SocialNavBar.js
+++ src/components/SocialNavBar.js
@@ -1,13 +1,15 @@
 import React from "react";
+import { AiOutlineLinkedin, AiFillGithub, AiOutlineMail } from 'react-icons/ai';
+// TiSocialLinkedinCircular
 
 function SocialNavBar() {
     return (
         <div id='social-nav-bar'>
-            <a href="https://www.linkedin.com/in/hannahmccullough/" target='blank'>LinkedIn</a>
+            <a href="https://www.linkedin.com/in/hannahmccullough/" target='blank'><AiOutlineLinkedin /></a>
             <hr className='social-hr' />
-            <a href="https://github.com/hmccull" target='blank'>GitHub</a>
+            <a href="https://github.com/hmccull" target='blank'><AiFillGithub /></a>
             <hr className='social-hr' />
-            <a href="mailto:mccullough.hannah@outlook.com">Email</a>
+            <a href="mailto:mccullough.hannah@outlook.com"><AiOutlineMail /></a>
             <hr className='social-hr' />
         </div>
     )
