7a2dbd476c6236364a00b0ee1284fcf00d9c126a
Hannah McCullough <hmac091@gmail.com>
2022-05-04 18:42:35 -0700

    vertical carousel added, needs styled

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
 import Housd from "./Housd";
 import StrengthTrainer from "./StrengthTrainer";
+
 
 function Projects() {
     return (
         <div id='projects'>
             <div className='left-section-header'>Projects</div>
             <hr />
-                <StrengthTrainer />
-                <Housd />
+            <Carousel
+                axis='vertical'
+                swipeable
+                verticalSwipe="natural"
+                infiniteLoop
+                autoFocus={true}
+                // showStatus={false}
+            >
+                <div>
+                    <StrengthTrainer />
+                </div>
+                <div>
+                    <Housd />
+                </div>
+            </Carousel>
         </div>
     )
 }
