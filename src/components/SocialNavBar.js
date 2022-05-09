a02b10ad763d80ea630d052fcbc9fdb389bdbc60
Hannah McCullough <mccullough.hannah@outlook.com>
2022-05-09 16:42:03 -0700

    css updates

--- src/components/SocialNavBar.js
+++ src/components/SocialNavBar.js
@@ -5,11 +5,11 @@ import { AiOutlineLinkedin, AiFillGithub, AiOutlineMail } from 'react-icons/ai';
 function SocialNavBar() {
     return (
         <div id='social-nav-bar'>
-            <a href='https://www.linkedin.com/in/hannahmccullough/' target='blank'><AiOutlineLinkedin size={35} /></a>
+            <a href='https://www.linkedin.com/in/hannahmccullough/' target='blank'><AiOutlineLinkedin size={20} /></a>
             <hr />
-            <a href='https://github.com/hmccull' target='blank'><AiFillGithub size={35} /></a>
+            <a href='https://github.com/hmccull' target='blank'><AiFillGithub size={20} /></a>
             <hr />
-            <a href='mailto:mccullough.hannah@outlook.com'><AiOutlineMail size={35} /></a>
+            <a href='mailto:mccullough.hannah@outlook.com'><AiOutlineMail size={20} /></a>
             <hr />
         </div>
     )
