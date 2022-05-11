6a96b75ab7c020682b113309a2359dca0f0ba0b8
Hannah McCullough <mccullough.hannah@outlook.com>
2022-05-10 17:31:24 -0700

    css updates

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,7 +7,6 @@ function NavBar() {
                 <li><a href='#home'>Home</a></li>
                 <li><a href='#about'>About</a></li>
                 <li><a href='#projects'>Projects</a></li>
-                <li><a href='#contact'>Contact</a></li>
                 <li><a href='./HannahMcCulloughResume.pdf' target='blank'>Resume</a></li>
             </ul>
         </div>
