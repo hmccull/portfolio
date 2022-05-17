6161e43728042c1db2dbca105eb495fb4105ff1a
Hannah McCullough <mccullough.hannah@outlook.com>
2022-05-16 17:21:39 -0700

    nav bar and project css adjustments

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,10 +3,10 @@ import React from 'react';
 function NavBar() {
     return (
         <div id='nav-bar'>
-            <a href='#home'>Home</a>
-            <a href='#about'>About</a>
-            <a href='#projects'>Projects</a>
             <a href='./HannahMcCulloughResume.pdf' target='blank'>Resume</a>
+            <a href='#projects'>Projects</a>
+            <a href='#about'>About</a>
+            <a href='#home'>Home</a>
         </div>
     )
 }
