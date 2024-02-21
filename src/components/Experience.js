067584dca5569914156f2f15025cc097c29e52f4
Hannah McCullough <hmccull@Hannahs-MacBook-Pro.local>
2024-02-21 10:55:59 -0700

    update professional experience

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -13,27 +13,27 @@ function Experience() {
             <hr className='section-hr' />
             <div id='experience-content'>
                 <p id='experience-text'>
-                    <b>Synapse Financial Technologies</b> <br />
-                    <b>Software Engineer II</b> <br />
-                    <i>Remote | 07/2022 - 10/2023</i> <br />
-                    ● Served as team lead providing technical guidance, mentorship, and knowledge to teammates <br />
-                    ● Implemented client direct requests, quick turnaround feature enhancements in Python RESTful APIs <br />
-                    ● Used strong troubleshooting and bug resolution abilities to quickly rectify production API issues using DataDog, EKS, & Postman <br />
-                    ● Managed internal & external developer documentation in Confluence that provided a deeper understanding of the technical side of the products & implementations <br />
-                    ● Provided consistent results via strong program design & management skills utilizing Jira & LucidChart <br />
-                    ● Improved cross-team processes by providing feedback and alternative solutions to leadership, which
-                    positively impacted client experience
+                    <b>Freelance</b> <br />
+                    <b>Web Developer/Designer</b> <br />
+                    <i>Phoenix, AZ | 01/2018 - Present</i> <br />
+                    ● Currently developing a full-stack application with extensive user capabilities and public property data utilizing React, JavaScript, TypeScript, Rails, & Active Record
+                    ● Modeled custom database schema and REST API with Ruby on Rails, Active Record, & PostgresSQL <br />
+                    ● Utilized Auth & bcrypt to authenticate user login and authorize user functionality based on user type <br />
+                    ● Designed websites for small businesses using content management systems with enhanced UI/UX, resulting in increased traffic and leads
                 </p>
             </div>
             <div id='experience-content'>
-                <p id='experience-text'>
-                    <b>Freelance</b> <br />
-                    <b>Web Developer/Designer</b> <br />
-                    <i>Phoenix, AZ | 01/2018 - 07/2022</i> <br />
-                    ● Modeled custom database schema and REST API with Active Record, Postgres, and Rails <br />
-                    ● Utilized Auth & bcrypt to authenticate user login and authorize user functionality based on user type <br />
-                    ● Designed websites for small businesses using content management systems with enhanced UI/UX,resulting in increased traffic and leads
-                </p>
+              <p id='experience-text'>
+                <b>Synapse Financial Technologies</b> <br />
+                <b>Software Engineer II</b> <br />
+                <i>Remote | 07/2022 - 10/2023</i> <br />
+                ● Served as team lead providing technical guidance, mentorship, and knowledge to teammates <br />
+                ● Implemented client direct request, quick turnaround feature enhancements in Python RESTful APIs <br />
+                ● Used strong troubleshooting and bug resolution abilities to quickly rectify production API issues using DataDog, EKS, and Postman <br />
+                ● Managed internal & external developer documentation in Confluence that provided a deeper understanding of the technical side of the products & implementations <br />
+                ● Provided consistent results via strong program design & management skills utilizing Jira & LucidChart <br />
+                ● Improved cross-team processes by providing feedback and alternative solutions to leadership, which positively impacted client experience integration changes
+              </p>
             </div>
             <div id='experience-content'>
                 <p id='experience-text'>
