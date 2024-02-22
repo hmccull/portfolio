ea5c61da49d610c942e7d0c6a9f67c633bad87c0
Hannah McCullough <hmccull@Hannahs-MacBook-Pro.local>
2024-02-21 17:39:05 -0700

    update profile pic

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -12,15 +12,15 @@ function Experience() {
             <div className='left-section-header'>Professional Experience</div>
             <hr className='section-hr' />
             <div id='experience-content'>
-                <p id='experience-text'>
-                    <b>Freelance</b> <br />
-                    <b>Web Developer/Designer</b> <br />
-                    <i>Phoenix, AZ | 01/2018 - Present</i> <br />
-                    ● Currently developing a full-stack application with extensive user capabilities and public property data utilizing React, JavaScript, TypeScript, Rails, & Active Record
-                    ● Modeled custom database schema and REST API with Ruby on Rails, Active Record, & PostgresSQL <br />
-                    ● Utilized Auth & bcrypt to authenticate user login and authorize user functionality based on user type <br />
-                    ● Designed websites for small businesses using content management systems with enhanced UI/UX, resulting in increased traffic and leads
-                </p>
+              <p id='experience-text'>
+                <b>Freelance</b> <br />
+                <b>Web Developer/Designer</b> <br />
+                <i>Phoenix, AZ | 01/2018 - Present</i> <br />
+                ● Currently developing a full-stack application with extensive user capabilities and public property data utilizing React, JavaScript, TypeScript, Rails, & Active Record
+                ● Modeled custom database schema and REST API with Ruby on Rails, Active Record, & PostgresSQL <br />
+                ● Utilized Auth & bcrypt to authenticate user login and authorize user functionality based on user type <br />
+                ● Designed websites for small businesses using content management systems with enhanced UI/UX, resulting in increased traffic and leads
+              </p>
             </div>
             <div id='experience-content'>
               <p id='experience-text'>
@@ -36,13 +36,13 @@ function Experience() {
               </p>
             </div>
             <div id='experience-content'>
-                <p id='experience-text'>
-                    <b>USAA</b> <br />
-                    <b>Software Engineer II</b> <br />
-                    <i>Phoenix, AZ | 09/2018 - 11/2021</i> <br />
-                    ● Served as a department facilitator in the Pulse initiative by communicating employee feedback & innovation to executives <br />
-                    ● Regularly facilitated team meetings, which resulted in an overall increase in team metrics
-                </p>
+              <p id='experience-text'>
+                <b>USAA</b> <br />
+                <b>Software Engineer II</b> <br />
+                <i>Phoenix, AZ | 09/2018 - 11/2021</i> <br />
+                ● Served as a department facilitator in the Pulse initiative by communicating employee feedback & innovation to executives <br />
+                ● Regularly facilitated team meetings, which resulted in an overall increase in team metrics
+              </p>
             </div>
             </Collapse>
         </div>
