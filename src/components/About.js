9cffe937aabda8660c2981f2767d3b384cb27c5d
Hannah McCullough <hmccull@Hannahs-MacBook-Pro.local>
2024-02-22 14:17:26 -0700

    CSS adjustments

--- src/components/About.js
+++ src/components/About.js
@@ -3,23 +3,23 @@ import Collapse from '@mui/material/Collapse';
 
 function About() {
 
-    return (
-        <div id='about'>
-          <Collapse 
-              direction='up' 
-              in
-          >
-          <div className='right-section-header'>About</div>
-          <hr className='section-hr' />
-          <div id='about-content'>
-            <img id='prof-pic' src='./profPic.png' alt='Hannah McCullough' />
-            <p id='about-text'>
-                I'm an innovative full-stack software engineer with skills in building applications & APIs with a user-experience approach. Having a curious mind and a background in customer service, management, and athletics, I bring strong skills in client relations, collaboration, and leadership.
-            </p>
-          </div>
-          </Collapse>
-        </div>
-    )
+  return (
+    <div id='about'>
+      <Collapse 
+          direction='up' 
+          in
+      >
+      <div className='right-section-header'>About</div>
+      <hr className='section-hr' />
+      <div id='about-content'>
+        <img id='prof-pic' src='./profPic.png' alt='Hannah McCullough' />
+        <p id='about-text'>
+            I'm an innovative full-stack software engineer with skills in building applications & APIs with a user-experience approach. Having a curious mind and a background in customer service, management, and athletics, I bring strong skills in client relations, collaboration, and leadership.
+        </p>
+      </div>
+      </Collapse>
+    </div>
+  )
 }
 
 export default About;
--- src/components/Education.js
+++ src/components/Education.js
@@ -3,38 +3,38 @@ import Collapse from '@mui/material/Collapse';
 
 function Education() {
 
-    return (
-        <div id='education'>
-            <Collapse 
-                direction='up' 
-                in
-            >
-            <div className='right-section-header'>Education</div>
-            <hr className='section-hr' />
-            <div id='education-content'>
-                <span id='education-text'>
-                    <b>Flatiron School</b> <br />
-                    <i>11/2021 - 03/2022</i> <br />
-                    Certificate <br />
-                    Full-stack Software Engineering
-                </span>
-                <span id='education-text'>
-                    <b>Scottsdale Community College</b> <br />
-                    <i>08/2018 - 12/2022</i> <br />
-                    Assocates in Applied Science<br />
-                    Web Development <br />
-                    Web Design
-                </span>
-                <span id='education-text' style={{ paddingRight: 0, paddingLeft: 50 }}>
-                    <b>Saint Francis University/Lake Erie College</b> <br />
-                    <i>08/2010 - 05/2014</i> <br />
-                    Bachelor of Arts in Psychology <br />
-                    Athlete
-                </span>
-            </div>
-            </Collapse>
+  return (
+    <div id='education'>
+      <Collapse 
+          direction='up' 
+          in
+      >
+        <div className='right-section-header'>Education</div>
+        <hr className='section-hr' />
+        <div id='education-content'>
+          <span id='education-text'>
+            <p><b>Flatiron School</b></p>
+            <p><i>11/2021 - 03/2022</i></p>
+            <p>Certificate</p>
+            <p>Full-stack Software Engineering</p>
+          </span>
+          <span id='education-text'>
+            <p><b>Scottsdale Community College</b></p>
+            <p><i>08/2018 - 12/2022</i></p>
+            <p>Assocates in Applied Science</p>
+            <p>Web Development</p>
+            <p>Web Design</p>
+          </span>
+          <span id='education-text' style={{ paddingRight: 0 }}>
+            <p><b>Saint Francis University/<br />Lake Erie College</b></p>
+            <p><i>08/2010 - 05/2014</i></p>
+            <p>Bachelor of Arts in Psychology</p>
+            <p>Athlete</p>
+          </span>
         </div>
-    )
+      </Collapse>
+    </div>
+  )
 }
 
 export default Education;
